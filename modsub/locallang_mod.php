bc66d3a8b32094204045b34e66c6343ccfa5a8bc
Fabien Udriot <fabien.udriot@ecodev.ch>
2015-03-19 11:54:45 +0100

    [BUGFIX] Disable French

--- modsub/locallang_mod.php
+++ modsub/locallang_mod.php
@@ -7,33 +7,28 @@ $LOCAL_LANG = Array (
 		'mlang_tabs_tab' => 'phpMyAdmin',
 	),
 	'dk' => Array (
-		'mlang_labels_tablabel' => 'Tredjeparts database administrationsværktøj, phpMyAdmin',
-		'mlang_labels_tabdescr' => 'Integreret \'Tredje-parts modul\' - det populære phpMyAdmin interface. Dette giver dig helt grundlæggende adgang til databasens tabeller. Adgangen til phpMyAdmin er kontrolleret af TYPO3. Men dette er også den eneste styring overhovedet - phpMyAdmin giver dig tilladelse til at ændre hvadend du prøver på, så vær forsigtig!<br /><em>Adgang kun for \'admin\' brugere!</em>',
+		'mlang_labels_tablabel' => 'Tredjeparts database administrationsvï¿½rktï¿½j, phpMyAdmin',
+		'mlang_labels_tabdescr' => 'Integreret \'Tredje-parts modul\' - det populï¿½re phpMyAdmin interface. Dette giver dig helt grundlï¿½ggende adgang til databasens tabeller. Adgangen til phpMyAdmin er kontrolleret af TYPO3. Men dette er ogsï¿½ den eneste styring overhovedet - phpMyAdmin giver dig tilladelse til at ï¿½ndre hvadend du prï¿½ver pï¿½, sï¿½ vï¿½r forsigtig!<br /><em>Adgang kun for \'admin\' brugere!</em>',
 		'mlang_tabs_tab' => 'phpMyAdmin',
 	),
 	'de' => Array (
 		'mlang_labels_tablabel' => 'Datenbank Administration von Dritt-Hersteller, phpMyAdmin',
-		'mlang_labels_tabdescr' => 'Integriertes \'Dritt-Hersteller-Modul\' - die bekannte phpMyAdmin Oberfläche. Diese erlaubt den grundlegenden Zugriff auf die Tabellen der Datenbank. Der Zugriff auf phpMyAdmin wird durch TYPO3 authentifiziert. Dies ist schon die einzige Einschränkung - phpMyAdmin bietet seinen kompletten Funktionsumfang, wodurch Sie bei Ihrer Arbeit mit diesem entsprechend vorsichtig seien sollten!<br /><em>Zugriff nur für Administratoren!</em>',
+		'mlang_labels_tabdescr' => 'Integriertes \'Dritt-Hersteller-Modul\' - die bekannte phpMyAdmin Oberflï¿½che. Diese erlaubt den grundlegenden Zugriff auf die Tabellen der Datenbank. Der Zugriff auf phpMyAdmin wird durch TYPO3 authentifiziert. Dies ist schon die einzige Einschrï¿½nkung - phpMyAdmin bietet seinen kompletten Funktionsumfang, wodurch Sie bei Ihrer Arbeit mit diesem entsprechend vorsichtig seien sollten!<br /><em>Zugriff nur fï¿½r Administratoren!</em>',
 		'mlang_tabs_tab' => 'phpMyAdmin',
 	),
 	'no' => Array (
-		'mlang_labels_tablabel' => 'Tredjeparts database administrasjonsverktøy, phpMyAdmin',
-		'mlang_labels_tabdescr' => 'Integrert tredjeparts modul - det populære phpMyAdmin grensesnittet. Dette gir deg helt grunnleggende adgang til databasens tabeller. Adgangen til phpMyAdmin er kontrollert av TYPO3. Dette er også den eneste styringen overhode - phpMyAdmin gir deg tillatelse til å endre hva som helst, så vær forsiktig!<br /><em>Adgang kun for \'admin\' brukere!</em>',
+		'mlang_labels_tablabel' => 'Tredjeparts database administrasjonsverktï¿½y, phpMyAdmin',
+		'mlang_labels_tabdescr' => 'Integrert tredjeparts modul - det populï¿½re phpMyAdmin grensesnittet. Dette gir deg helt grunnleggende adgang til databasens tabeller. Adgangen til phpMyAdmin er kontrollert av TYPO3. Dette er ogsï¿½ den eneste styringen overhode - phpMyAdmin gir deg tillatelse til ï¿½ endre hva som helst, sï¿½ vï¿½r forsiktig!<br /><em>Adgang kun for \'admin\' brukere!</em>',
 		'mlang_tabs_tab' => 'phpMyAdmin',
 	),
 	'it' => Array (
 		'mlang_labels_tablabel' => 'phpMyAdmin, tool per l\'amministrazione di database',
-		'mlang_labels_tabdescr' => 'phpMyAdmin è un tool (sviluppato da terzi) molto diffuso che consente l\'accesso a basso livello a tutte le tabelle del database. L\'autenticazione è gestita da TYPO3. phpMyAdmin consente di modificare qualunque cosa, senza controllo di TYPO3, quindi è necessario prestare particolare attenzione alle modifiche che si compiono.<br /><em>Accesso riservato ai soli utenti \'admin\'!</em>',
-		'mlang_tabs_tab' => 'phpMyAdmin',
-	),
-	'fr' => Array (
-		'mlang_labels_tablabel' => 'Administration tiers de la base de données, phpMyAdmin',
-		'mlang_labels_tabdescr' => 'Le célébre outil d\'administration pour base de données mySQL est maintenant intégré dans TYPO3.',
+		'mlang_labels_tabdescr' => 'phpMyAdmin ï¿½ un tool (sviluppato da terzi) molto diffuso che consente l\'accesso a basso livello a tutte le tabelle del database. L\'autenticazione ï¿½ gestita da TYPO3. phpMyAdmin consente di modificare qualunque cosa, senza controllo di TYPO3, quindi ï¿½ necessario prestare particolare attenzione alle modifiche che si compiono.<br /><em>Accesso riservato ai soli utenti \'admin\'!</em>',
 		'mlang_tabs_tab' => 'phpMyAdmin',
 	),
 	'es' => Array (
-		'mlang_labels_tablabel' => 'Administración de base de datos de un tercero, phpMyAdmin',
-		'mlang_labels_tabdescr' => 'Con el módulo incluido de un tercero, el popular phpMyAdmin, permite acceso directo a las tablas de las base de datos. El acceso a phpMyAdmin es autentificado por medio de TYPO3. Salvo ese detalle TYPO3 no interfiere para nada - phpMyAdmin le permite el modificar lo que deseee, por tanto ¡tenga cuidado!<br /><em>¡Acceso solamente para usuarios \'administrador\'!</em>',
+		'mlang_labels_tablabel' => 'Administraciï¿½n de base de datos de un tercero, phpMyAdmin',
+		'mlang_labels_tabdescr' => 'Con el mï¿½dulo incluido de un tercero, el popular phpMyAdmin, permite acceso directo a las tablas de las base de datos. El acceso a phpMyAdmin es autentificado por medio de TYPO3. Salvo ese detalle TYPO3 no interfiere para nada - phpMyAdmin le permite el modificar lo que deseee, por tanto ï¿½tenga cuidado!<br /><em>ï¿½Acceso solamente para usuarios \'administrador\'!</em>',
 		'mlang_tabs_tab' => 'phpMyAdmin',
 	),
 	'nl' => Array (
@@ -42,54 +37,54 @@ $LOCAL_LANG = Array (
 		'mlang_tabs_tab' => 'phpMyAdmin',
 	),
 	'cz' => Array (
-		'mlang_labels_tablabel' => 'Administrance databáze od tøetí strany, phpMyAdmin',
-		'mlang_labels_tabdescr' => 'Integrovaný modul \'tøetí strany\', populární phpMyAdmin interafce. Umožòuje úplný pøístup k databázovým tabulkám. Pøístup k phpMyAdmin je autentifikován pøes TYPO3. Ale toto je jediná interference - phpMyAdmin umožní cokoli si usmyslíte, proto buïte opatrní!<br /><em>Pøístup pouze pro \'admin\' uživatele!</em>',
+		'mlang_labels_tablabel' => 'Administrance databï¿½ze od tï¿½etï¿½ strany, phpMyAdmin',
+		'mlang_labels_tabdescr' => 'Integrovanï¿½ modul \'tï¿½etï¿½ strany\', populï¿½rnï¿½ phpMyAdmin interafce. Umoï¿½ï¿½uje ï¿½plnï¿½ pï¿½ï¿½stup k databï¿½zovï¿½m tabulkï¿½m. Pï¿½ï¿½stup k phpMyAdmin je autentifikovï¿½n pï¿½es TYPO3. Ale toto je jedinï¿½ interference - phpMyAdmin umoï¿½nï¿½ cokoli si usmyslï¿½te, proto buï¿½te opatrnï¿½!<br /><em>Pï¿½ï¿½stup pouze pro \'admin\' uï¿½ivatele!</em>',
 		'mlang_tabs_tab' => 'phpMyAdmin',
 	),
 	'pl' => Array (
-		'mlang_labels_tablabel' => 'Zewnêtrzny modu³ do administracji baz±, phpMyAdmin',
-		'mlang_labels_tabdescr' => 'Zintegrowany \'Zewnêtrzny\' modu³, popularnego interfejsu phpMyAdmin. Zapewnia on dostêp do tablic na najni¿szym poziomie. Autentykacja jest realizowana przez TYPO3. Ten modu³ pozwala na nieograniczon± ingerencjê w bazê. Zalecana du¿a ostro¿no¶æ. <br /><em>Dostêp tylko dla administratorów!<em>',
+		'mlang_labels_tablabel' => 'Zewnï¿½trzny moduï¿½ do administracji bazï¿½, phpMyAdmin',
+		'mlang_labels_tabdescr' => 'Zintegrowany \'Zewnï¿½trzny\' moduï¿½, popularnego interfejsu phpMyAdmin. Zapewnia on dostï¿½p do tablic na najniï¿½szym poziomie. Autentykacja jest realizowana przez TYPO3. Ten moduï¿½ pozwala na nieograniczonï¿½ ingerencjï¿½ w bazï¿½. Zalecana duï¿½a ostroï¿½noï¿½ï¿½. <br /><em>Dostï¿½p tylko dla administratorï¿½w!<em>',
 		'mlang_tabs_tab' => 'phpMyAdmin',
 	),
 	'si' => Array (
 		'mlang_labels_tablabel' => 'phpMyAdmin, orodje za administracijo MySQL baze',
-		'mlang_labels_tabdescr' => 'Na voljo vam je popularno orodje za administracijo MySQL strežnika. Ker omogoèa popoln nadzor nad podatkovno bazo, zahteva uporaba nekaj previdnosti in znanja. Avtorizacija dostopa poteka preko TYPO3.<br /><em>Uporaba je dovoljena samo \'admin\' uporabnikom!</em>',
+		'mlang_labels_tabdescr' => 'Na voljo vam je popularno orodje za administracijo MySQL streï¿½nika. Ker omogoï¿½a popoln nadzor nad podatkovno bazo, zahteva uporaba nekaj previdnosti in znanja. Avtorizacija dostopa poteka preko TYPO3.<br /><em>Uporaba je dovoljena samo \'admin\' uporabnikom!</em>',
 		'mlang_tabs_tab' => 'phpMyAdmin',
 	),
 	'fi' => Array (
 		'mlang_labels_tablabel' => 'Tietokanta hallinta, phpMyAdmin',
-		'mlang_labels_tabdescr' => 'TYPO3n kanssa integroitu kolmannen osapuolen tietokanta hallinta. <br /><em>Sallittu vain \'Admin\'  käyttäjille.</em> ',
+		'mlang_labels_tabdescr' => 'TYPO3n kanssa integroitu kolmannen osapuolen tietokanta hallinta. <br /><em>Sallittu vain \'Admin\'  kï¿½yttï¿½jille.</em> ',
 		'mlang_tabs_tab' => 'phpMyAdmin',
 	),
 	'tr' => Array (
-		'mlang_labels_tablabel' => 'Üçüncü-Parti veritabaný yönetimi, phpMyAdmin',
-		'mlang_labels_tabdescr' => 'Bütünleþmiþ \'Üçüncü-parti modülü\', popüler phpMyAdmin arabirimi. Bu izinler temel seviyede veritabaný tablolarýna eriþimi saðlar. PhpMyAdmin e eriþim TYPO3 içinden doðrulandýrýlýyor. Fakat bu sadece herhangi bir kýsýtlamadýr - phpMyAdmin neyi isterseniz onu modifiye etmenize izin verir!<br /><em>Sadece kullanýcýlarýn \'yönetim\' için geçiþi!</em>',
+		'mlang_labels_tablabel' => 'ï¿½ï¿½ï¿½ncï¿½-Parti veritabanï¿½ yï¿½netimi, phpMyAdmin',
+		'mlang_labels_tabdescr' => 'Bï¿½tï¿½nleï¿½miï¿½ \'ï¿½ï¿½ï¿½ncï¿½-parti modï¿½lï¿½\', popï¿½ler phpMyAdmin arabirimi. Bu izinler temel seviyede veritabanï¿½ tablolarï¿½na eriï¿½imi saï¿½lar. PhpMyAdmin e eriï¿½im TYPO3 iï¿½inden doï¿½rulandï¿½rï¿½lï¿½yor. Fakat bu sadece herhangi bir kï¿½sï¿½tlamadï¿½r - phpMyAdmin neyi isterseniz onu modifiye etmenize izin verir!<br /><em>Sadece kullanï¿½cï¿½larï¿½n \'yï¿½netim\' iï¿½in geï¿½iï¿½i!</em>',
 		'mlang_tabs_tab' => 'phpMyAdmin',
 	),
 	'se' => Array (
 		'mlang_labels_tablabel' => 'Administration av tredje parts databas, phpMyAdmin',
-		'mlang_labels_tabdescr' => 'Detta är en integrerad tredje parts databas, användargränssnitt till den populära phpMyAdmin. Den ger möjlighet att nå basen i tabellerna. phpMyAdmin-åtkomsten bekräftas via Typo3. Men var försiktig, för detta är det enda gränssnittet, och phpMyAdmin låter dej ändra vad som helst! <br /><em>Tillgång endast för \'admin\'-användar!</em>',
+		'mlang_labels_tabdescr' => 'Detta ï¿½r en integrerad tredje parts databas, anvï¿½ndargrï¿½nssnitt till den populï¿½ra phpMyAdmin. Den ger mï¿½jlighet att nï¿½ basen i tabellerna. phpMyAdmin-ï¿½tkomsten bekrï¿½ftas via Typo3. Men var fï¿½rsiktig, fï¿½r detta ï¿½r det enda grï¿½nssnittet, och phpMyAdmin lï¿½ter dej ï¿½ndra vad som helst! <br /><em>Tillgï¿½ng endast fï¿½r \'admin\'-anvï¿½ndar!</em>',
 		'mlang_tabs_tab' => 'phpMyAdmin',
 	),
 	'pt' => Array (
 		'mlang_labels_tablabel' => 'phpMyAdmin - BD Admin Externa',
-		'mlang_labels_tabdescr' => '\'Módulo de terceiros\' integrado, o popular interface phpMyAdmin. Este módulo permite acesso básico às tabelas da base de dados. O acesso ao phpMyAdmin é autenticado através do TYPO3. Mas esta autenticação é somente interferência - phpMyAdmin permite-lhe modificar tudo o que quiser, por isso seja cuidadoso!<br /><em>Acesso só para utilizadores \'admin\'!</em>',
+		'mlang_labels_tabdescr' => '\'Mï¿½dulo de terceiros\' integrado, o popular interface phpMyAdmin. Este mï¿½dulo permite acesso bï¿½sico ï¿½s tabelas da base de dados. O acesso ao phpMyAdmin ï¿½ autenticado atravï¿½s do TYPO3. Mas esta autenticaï¿½ï¿½o ï¿½ somente interferï¿½ncia - phpMyAdmin permite-lhe modificar tudo o que quiser, por isso seja cuidadoso!<br /><em>Acesso sï¿½ para utilizadores \'admin\'!</em>',
 		'mlang_tabs_tab' => 'phpMyAdmin',
 	),
 	'ru' => Array (
-		'mlang_labels_tablabel' => 'Àäìèíèñòðàöèÿ áàçû äàííûõ, phpMyAdmin',
+		'mlang_labels_tablabel' => 'ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½, phpMyAdmin',
 		'mlang_tabs_tab' => 'phpMyAdmin',
 	),
 	'ro' => Array (
-		'mlang_labels_tablabel' => 'Administrare bazã de date, phpMyAdmin',
-		'mlang_labels_tabdescr' => 'Modulul extern integrat, de mare popularitate, phpMyAdmin. Acesta permite acces de nivel inferior la tabelele din baza de date. Autentificarea la phpMyAdmin este realizatã de cãtre TYPO3. Dar aceasta este singura interferenþã - phpMyAdmin vã permite sã modificaþi tot ce doriþi, deci fiþi atenþi!<br /><em>Acces permis doar administratorilor!</em>',
+		'mlang_labels_tablabel' => 'Administrare bazï¿½ de date, phpMyAdmin',
+		'mlang_labels_tabdescr' => 'Modulul extern integrat, de mare popularitate, phpMyAdmin. Acesta permite acces de nivel inferior la tabelele din baza de date. Autentificarea la phpMyAdmin este realizatï¿½ de cï¿½tre TYPO3. Dar aceasta este singura interferenï¿½ï¿½ - phpMyAdmin vï¿½ permite sï¿½ modificaï¿½i tot ce doriï¿½i, deci fiï¿½i atenï¿½i!<br /><em>Acces permis doar administratorilor!</em>',
 		'mlang_tabs_tab' => 'phpMyAdmin',
 	),
 	'ch' => Array (
 	),
 	'sk' => Array (
-		'mlang_labels_tablabel' => 'Administrácia databázy od tretej strany, phpMyAdmin',
-		'mlang_labels_tabdescr' => 'Integrovaný modul \'tretej strany\', populárny phpMyAdmin interface. Umožòuje úplný prístup k databázovým tabu¾kám. Prístup k phpMyAdmin je autentifikovaný prostredníctvom Typo3. Toto je jediný rozdiel - phpMyAdmin umožòuje naèo pomyslíš, preto buï ve¾mi opatrný!<br /><em>Prístup len pre \'admin\' užívate¾a!</em>',
+		'mlang_labels_tablabel' => 'Administrï¿½cia databï¿½zy od tretej strany, phpMyAdmin',
+		'mlang_labels_tabdescr' => 'Integrovanï¿½ modul \'tretej strany\', populï¿½rny phpMyAdmin interface. Umoï¿½ï¿½uje ï¿½plnï¿½ prï¿½stup k databï¿½zovï¿½m tabuï¿½kï¿½m. Prï¿½stup k phpMyAdmin je autentifikovanï¿½ prostrednï¿½ctvom Typo3. Toto je jedinï¿½ rozdiel - phpMyAdmin umoï¿½ï¿½uje naï¿½o pomyslï¿½, preto buï¿½ veï¿½mi opatrnï¿½!<br /><em>Prï¿½stup len pre \'admin\' uï¿½ï¿½vateï¿½a!</em>',
 		'mlang_tabs_tab' => 'phpMyAdmin',
 	),
 	'lt' => Array (
@@ -101,8 +96,8 @@ $LOCAL_LANG = Array (
 		'mlang_tabs_tab' => 'phpMyAdmin',
 	),
 	'hu' => Array (
-		'mlang_labels_tablabel' => 'Külsõ adatbázis kezelõ, phpMyAdmin',
-		'mlang_labels_tabdescr' => 'Beépített külsõ modul, a népszerû phpMyAdmin felület. Ez alacsony szintû adatbázis elérést teszt lehetõvé. !<br /><em>Csak \'admin\' felhasználóknak elérhetõ!</em>',
+		'mlang_labels_tablabel' => 'Kï¿½lsï¿½ adatbï¿½zis kezelï¿½, phpMyAdmin',
+		'mlang_labels_tabdescr' => 'Beï¿½pï¿½tett kï¿½lsï¿½ modul, a nï¿½pszerï¿½ phpMyAdmin felï¿½let. Ez alacsony szintï¿½ adatbï¿½zis elï¿½rï¿½st teszt lehetï¿½vï¿½. !<br /><em>Csak \'admin\' felhasznï¿½lï¿½knak elï¿½rhetï¿½!</em>',
 		'mlang_tabs_tab' => 'phpMyAdmin',
 	),
 	'gl' => Array (
@@ -119,12 +114,12 @@ $LOCAL_LANG = Array (
 	),
 	'br' => Array (
 		'mlang_labels_tablabel' => 'phpMyAdmin - BD Admin Externa',
-		'mlang_labels_tabdescr' => '\'Módulo de terceiros\' integrado, a popular interface phpMyAdmin. Este módulo permite acesso básico às tabelas do banco de dados. O acesso ao phpMyAdmin é autenticado através do Typo3. Mas esta autenticação é a única interferência - phpMyAdmin permite-lhe modificar tudo o que quiser, por isso seja cuidadoso!<br /><em>Acesso só para usuários \'admin\'!</em>',
+		'mlang_labels_tabdescr' => '\'Mï¿½dulo de terceiros\' integrado, a popular interface phpMyAdmin. Este mï¿½dulo permite acesso bï¿½sico ï¿½s tabelas do banco de dados. O acesso ao phpMyAdmin ï¿½ autenticado atravï¿½s do Typo3. Mas esta autenticaï¿½ï¿½o ï¿½ a ï¿½nica interferï¿½ncia - phpMyAdmin permite-lhe modificar tudo o que quiser, por isso seja cuidadoso!<br /><em>Acesso sï¿½ para usuï¿½rios \'admin\'!</em>',
 		'mlang_tabs_tab' => 'phpMyAdmin',
 	),
 	'et' => Array (
-		'mlang_labels_tablabel' => 'Kolmanda osapoole andmebaasi administreerimise tööriist - phpMyAdmin',
-		'mlang_labels_tabdescr' => 'Integreeritud kolmanda osapoole moodul phpMyAdmin. See võimaldab ligipääsu kõigile andmebaasi tabelitele. phpMyAdmin-i ligipääs toimub läbi Typo3-e. Kuna see on ainus ligipääs ning läbi phpMyAdmin-i on võimalik muuta mida iganes soovite, siis olge palun ettevaatlik!!<br /><em>Ligipääs ainult \'admin\' kasutajatele!</em>',
+		'mlang_labels_tablabel' => 'Kolmanda osapoole andmebaasi administreerimise tï¿½ï¿½riist - phpMyAdmin',
+		'mlang_labels_tabdescr' => 'Integreeritud kolmanda osapoole moodul phpMyAdmin. See vï¿½imaldab ligipï¿½ï¿½su kï¿½igile andmebaasi tabelitele. phpMyAdmin-i ligipï¿½ï¿½s toimub lï¿½bi Typo3-e. Kuna see on ainus ligipï¿½ï¿½s ning lï¿½bi phpMyAdmin-i on vï¿½imalik muuta mida iganes soovite, siis olge palun ettevaatlik!!<br /><em>Ligipï¿½ï¿½s ainult \'admin\' kasutajatele!</em>',
 		'mlang_tabs_tab' => 'phpMyAdmin',
 	),
 	'ar' => Array (
